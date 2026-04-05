2bea88cd52b280e7390c210066399c1347472928
Harry H <them3m3ergod@gmail.com>
2026-04-05 11:56:08 +0100

    service file

--- src/p2fa/core/service.py
+++ src/p2fa/core/service.py
@@ -1,1 +1,36 @@
 # TOTP generation logic
+import pyotp
+
+def base_32(
+    secret: str,
+) -> str:
+    secret = secret.replace(
+        " ",
+        ""
+    ).upper()
+
+    # base32 strings MUST be a multiple of 8 characters
+    padding = len(
+        secret
+    ) % 8
+
+    # Adds the missing "=" padding
+    if padding:
+        secret += "=" * (8 - padding)
+
+    return secret
+
+
+def gen_code (
+        secret: str
+) -> str:
+
+    fixed_secret = base_32(
+        secret
+    )
+
+    totp = pyotp.TOTP(
+        fixed_secret
+    )
+    # Takes the 2fa secret and returns an otp (6 digit code)
+    return totp.now()
